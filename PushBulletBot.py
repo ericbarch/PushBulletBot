ee8df5a6d4549641a8ba62a108c69c158aa271db
Eric Barch <ericb@ericbarch.com>
2016-01-03 12:19:37 -0500

    run as cron

--- PushBulletBot.py
+++ PushBulletBot.py
@@ -1,51 +1,50 @@
 from pushbullet import Pushbullet
 import pywapi
 import time
-from time import sleep
+import sys
 from datetime import datetime
 import praw
 
-#Fill out this info
-weatherCode = 'ZIP'
+# weather config
+weatherCode = 'CODE'
 name = 'NAME'
 
-#Pushbullet API Key
+# pushbullet config
 api_key = 'KEY'
 
-def pushGoodMorning():
-	#PushBullet API
-	pb = Pushbullet(api_key)
+# setup pushbullet
+pb = Pushbullet(api_key)
 
-	#getting time
+# TODO: check snowboarding conditions
+# TODO: notify when stuff comes back in stock
+
+def push_weather():
+	# get the time
 	now = datetime.now().time()
-	#AM or PM
-	mornAfternoon = datetime.now().strftime('%p')
 
-	#Here we push the notifcation(s)
-	if now.hour == 7 and now.minute == 30:
-		#Getting the weather
-		weather_results = pywapi.get_weather_from_weather_com(weatherCode)
-		weather_saying = ("Today's forecast is " + weather_results['forecasts'][0]['day']['text'] + ".")
-		#pushing
-		push = pb.push_note(name + ", it's currently " + str(now.hour) + ":" + str(now.minute) + ".",  weather_saying)
-	else:
-                pass
+	# get the weather
+	weather_results = pywapi.get_weather_from_weather_com(weatherCode)
+	weather_saying = ("Today's forecast is " + weather_results['forecasts'][0]['day']['text'] + ".")
+	# push it
+	push = pb.push_note(name + ", it's currently " + str(now.hour) + ":" + str(now.minute) + ".",  weather_saying)
 
-	
-	if now.hour == 8 and now.minute == 15:
-		#getting reddit post title
-		user_agent = "Browser that loads top post in worldnews"
-		r = praw.Reddit(user_agent=user_agent)
-		submission = r.get_subreddit('worldnews').get_top_from_day(limit=1)
-		for x in submission:
-			topTitle = x.title.encode('utf-8')
-                        topLink = x.url.encode('utf-8')
-		#Pushing
-		push = pb.push_link(topTitle, topLink)
-	else:
-		pass
+def push_news():
+	# get top reddit post
+	user_agent = "Browser that loads top post in worldnews"
+	r = praw.Reddit(user_agent=user_agent)
+	submission = r.get_subreddit('worldnews').get_top_from_day(limit=1)
+	for x in submission:
+		topTitle = x.title.encode('utf-8')
+		topLink = x.url.encode('utf-8')
+	# push it
+	push = pb.push_link(topTitle, topLink)
 
-while True:
-	sleep(45)
-	pushGoodMorning()
+if len(sys.argv) != 2:
+	print "Usage: PushBulletBot.py <weather|news>"
+	sys.exit()
 
+if sys.argv[1] == 'weather':
+	push_weather()
+
+if sys.argv[1] == 'news':
+	push_news()
